3876063f43fea08f53f0db45c7c7e9413156ed84
Dean Galvin <deangalvin3@gmail.com>
2020-06-18 23:09:09 -0400

    remove arg to remove

--- main.go
+++ main.go
@@ -89,11 +89,8 @@ func rewriteEnv(in []string) {
 }
 
 func rewriteArgs(in []string) {
-	argToRemove := -1
 	for i, v := range in {
 		switch v {
-		case "-crf:0":
-			argToRemove = i
 		case "-progressurl", "-manifest_name", "-segment_list":
 			in[i+1] = strings.Replace(in[i+1], "http://127.0.0.1:32400", pmsInternalAddress, 1)
 		case "-loglevel", "-loglevel_plex":
